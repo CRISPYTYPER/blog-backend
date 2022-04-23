fcadc36caaf6b8a884c57e5f975fb32f97e9d194
CRISPYTYPER <jack3409@naver.com>
2022-04-23 16:50:49 +0900

    page 650

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -14,7 +14,7 @@ POST /api/posts
 {title,body}
 */
 
-exports.write = (ctx) => {
+export const write = (ctx) => {
     // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
     const { title, body } = ctx.request.body;
     postId += 1; // 기존 postId 값에 1을 더합니다.
@@ -27,14 +27,14 @@ exports.write = (ctx) => {
 GET /api/posts
 */
 
-exports.list = (ctx) => {
+export const list = (ctx) => {
     ctx.body = posts;
 };
 
 /* 특정 포스트 조회
     GET /api/posts/:id
 */
-exports.read = (ctx) => {
+export const read = (ctx) => {
     const { id } = ctx.params;
     // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
     // 비교할 p.id 값을 문자열로 변경해야 합니다.
@@ -54,7 +54,7 @@ exports.read = (ctx) => {
 DELETE /api/posts/:id
 */
 
-exports.remove = (ctx) => {
+export const remove = (ctx) => {
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인합니다.
     const index = posts.findIndex((p) => p.id.toString() === id);
@@ -76,7 +76,7 @@ PUT /api/posts/:id
 {title, body}
 */
 
-exports.replace = (ctx) => {
+export const replace = (ctx) => {
     // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인합니다.
@@ -102,7 +102,7 @@ exports.replace = (ctx) => {
 PATCH /api/posts/:id
 {title, body}
 */
-exports.update = (ctx) => {
+export const update = (ctx) => {
     // PATCH 메서드는 주어진 필드만 교체합니다.
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인합니다.
@@ -122,4 +122,3 @@ exports.update = (ctx) => {
     };
     ctx.body = posts[index];
 };
-
