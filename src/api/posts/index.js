c3d5165610a90fe2f960b4a41e6f156ba3029c93
CRISPYTYPER <jack3409@naver.com>
2022-04-26 19:30:15 +0900

    page 708

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,16 +1,21 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+// checkLoggedIn 미들웨어는 로그인 상태가 아니라면 401 HTTP Status를 반환하고,
+// 그렇지 않으면 그 다음 미들웨어들을 실행합니다.
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); // /api/posts/:id
 
 post.get('/', postsCtrl.read);
-post.delete('/', postsCtrl.remove);
-post.patch('/', postsCtrl.update);
+// checkLoggedIn 미들웨어는 로그인 상태가 아니라면 401 HTTP Status를 반환하고,
+// 그렇지 않으면 그 다음 미들웨어들을 실행합니다.
+post.delete('/', checkLoggedIn, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
