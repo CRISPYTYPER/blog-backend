cd59b0433ad7eb377449845f423fc510fd3b412b
CRISPYTYPER <jack3409@naver.com>
2022-04-26 20:00:18 +0900

    page 714

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -14,9 +14,9 @@ const post = new Router(); // /api/posts/:id
 post.get('/', postsCtrl.read);
 // checkLoggedIn 미들웨어는 로그인 상태가 아니라면 401 HTTP Status를 반환하고,
 // 그렇지 않으면 그 다음 미들웨어들을 실행합니다.
-post.delete('/', checkLoggedIn, postsCtrl.remove);
-post.patch('/', checkLoggedIn, postsCtrl.update);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
-posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+posts.use('/:id', postsCtrl.getPostById, post.routes());
 
 export default posts;
